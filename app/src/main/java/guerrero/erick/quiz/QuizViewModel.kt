959eb72c5fd598a4650f5e61e67445f84aeed0c5
JMGR <ingjmiguelgr@gmail.com>
2022-12-07 16:11:53 -0600

    preguntas corregidas

--- app/src/main/java/guerrero/erick/quiz/QuizViewModel.kt
+++ app/src/main/java/guerrero/erick/quiz/QuizViewModel.kt
@@ -9,11 +9,23 @@ private const val TAG = "QuizViewModel"
 public const val CURRENT_INDEX_KEY = "CURRENT_INDEX_KEY"
 class QuizViewModel(private val savedStateHandle:SavedStateHandle): ViewModel() {
     private val bancoPreguntas = listOf(
-        Pregunta(R.string.pregunta_chocolate,false),
-        Pregunta(R.string.pregunta_33,false),
-        Pregunta(R.string.pregunta_bajas, false),
-        Pregunta(R.string.pregunta_proyecto, true)
-    )
+        Pregunta(R.string.pregunta1,true),
+        Pregunta(R.string.pregunta2,false),
+        Pregunta(R.string.pregunta3,false),
+        Pregunta(R.string.pregunta4,false),
+        Pregunta(R.string.pregunta5,true),
+        Pregunta(R.string.pregunta6,true),
+        Pregunta(R.string.pregunta7,false),
+        Pregunta(R.string.pregunta8,true),
+        Pregunta(R.string.pregunta9,false),
+        Pregunta(R.string.pregunta10,false),
+        Pregunta(R.string.pregunta11,true),
+        Pregunta(R.string.pregunta12,true),
+        Pregunta(R.string.pregunta13,false),
+        Pregunta(R.string.pregunta14,false),
+        Pregunta(R.string.pregunta15,true),
+        )
+
     private var indice:Int
         get() = savedStateHandle.get(CURRENT_INDEX_KEY) ?: 0
         set(value) = savedStateHandle.set(CURRENT_INDEX_KEY,value)
