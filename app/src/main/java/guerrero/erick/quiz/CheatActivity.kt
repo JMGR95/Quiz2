0c2b8c55b83fc89b7d0c2bd8abc3c7fd1e52a0c0
JMGR <ingjmiguelgr@gmail.com>
2022-12-07 16:53:35 -0600

    Using intent extras Capitulo 7

--- app/src/main/java/guerrero/erick/quiz/CheatActivity.kt
+++ app/src/main/java/guerrero/erick/quiz/CheatActivity.kt
@@ -1,15 +1,43 @@
 package guerrero.erick.quiz
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import guerrero.erick.quiz.databinding.ActivityCheatBinding
 
+private const val EXTRA_ANSWER_IS_TRUE =
+    "guerrero.erick.quiz.answer_is_true"
+    //"com.bignerdranch.android.geoquiz.answer_is_true"
+
+
 class CheatActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityCheatBinding
+
+    private var answerIsTrue = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCheatBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        answerIsTrue = intent.getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false)
+
+        binding.showAnswerButton.setOnClickListener {
+            val answerText = when {
+                answerIsTrue -> R.string.true_button
+                else -> R.string.false_button
+            }
+            binding.answerTextView.setText(answerText)
+        }
+    }
+
+    companion object {
+        fun newIntent(packageContext: Context, answerIsTrue: Boolean): Intent {
+            return Intent(packageContext, CheatActivity::class.java).apply {
+                putExtra(EXTRA_ANSWER_IS_TRUE, answerIsTrue)
+            }
+        }
     }
 }
